fba9058608a06f8b7a48373bfb7bbf1a59749b45
EmyEmilia <108371605+EmyEmilia@users.noreply.github.com>
2022-12-18 18:20:28 +0200

    Create pj2.py
    Tells how many characters is on this string.

---
+++ pj2.py
@@ -0,0 +1,13 @@
+v= "Andreea" # the strings is used in ""
+print(len(v))  # len is a link to an object/string on ()
+child=[1,2,3,4,5,6,7]
+print(len(child))
+print(child[2])
+print(child[-1])
+print(v.index("e"))#is about the index in an variable
+sin=-10
+print(str(sin)  + "this is my favorit number")# if you put a number or samething near a string if you want to create samething with that
+print(abs(sin))# value of a string, if is negative it will turn positive
+print(pow(sin, 2))# its rize up as a power for a  variable base or a number of your choose.sin^2
+print(pow(10, 3))# 10^3
+print(max(2,5))# max or min function.
