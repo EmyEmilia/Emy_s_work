3c043d661a09ef182c8874ac75120a8524d63274
EmyEmilia <108371605+EmyEmilia@users.noreply.github.com>
2022-12-18 21:53:20 +0200

    Add files via upload

---
+++ py4.py
@@ -0,0 +1,20 @@
+monthConversion = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Nov": "November",
+    "Dec": "December",
+}
+print(monthConversion["Jan"])
+
+i=1
+while i<=10:
+    print(i)
+    i+=1
+print("Done with loop while")
